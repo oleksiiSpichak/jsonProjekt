8e269082b7fad4b63fe80e572d79adca9fc8815f
Zver <zveruga2103@gmail.com>
2018-03-06 21:34:56 +0200

    first commit

---
+++ src/com/company/IncomingServerMesage.java
@@ -0,0 +1,41 @@
+package com.company;
+
+/**
+ * Created by Zver on 05.03.2018.
+ */
+public class IncomingServerMesage {
+   private String accept, denay;
+   private Integer userId, chatId;
+
+   public String getAccept() {
+      return accept;
+   }
+
+   public String getDenay() {
+      return denay;
+   }
+
+   public Integer getUserId() {
+      return userId;
+   }
+
+   public Integer getChatId() {
+      return chatId;
+   }
+
+   public void setAccept(String accept) {
+      this.accept = accept;
+   }
+
+   public void setDenay(String denay) {
+      this.denay = denay;
+   }
+
+   public void setUserId(Integer userId) {
+      this.userId = userId;
+   }
+
+   public void setChatId(Integer chatId) {
+      this.chatId = chatId;
+   }
+}
